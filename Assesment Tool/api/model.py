ed2771fa3b2da60f83b1c911739b645d4f53921b
Sakshi Negi <77374152+Sakshi-Negi-2001@users.noreply.github.com>
2023-05-29 17:37:42 +0530

    Add files via upload

---
+++ Assesment Tool/api/model.py
@@ -0,0 +1,33 @@
+#from sklearn.neural_network import MLPRegressor
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import pickle
+
+data2 = pd.read_csv('Static5.csv')
+data2.drop(data2.iloc[:, 6:], inplace=True, axis=1)
+
+
+"""
+labels = np.array(data2['FOS'])
+data2 = data2.drop('FOS', axis=1)
+data2 = np.array(data2)
+
+x_train, x_test, y_train, y_test = train_test_split(data2, labels, test_size=0.20)
+nn = MLPRegressor(random_state=5, max_iter=84)
+nn.fit(x_train, y_train)
+
+pickle.dump(nn, open('model.pkl', 'wb'))
+print(nn.predict([[19, 30, 26, 30, 38]]))
+"""
+labels = np.array(data2['FOS'])
+data2 = data2.drop('FOS', axis=1)
+data2 = np.array(data2)
+
+x_train, x_test, y_train, y_test = train_test_split(data2, labels, test_size=0.20)
+lm = linear_model.LinearRegression()
+lm.fit(x_train, y_train)
+
+pickle.dump(lm, open('model.pkl', 'wb'))
+print(lm.predict([[19, 30, 26, 30, 38]]))
